592cef11d731bec3585cb282abbf3eb04c5d4c34
Mel <mmgravier@gmail.com>
2017-11-03 00:48:15 -0400

    first app dog years

--- My First App/ViewController.swift
+++ My First App/ViewController.swift
@@ -9,21 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var textField: UITextField!
     
     
     @IBOutlet weak var label: UILabel!
     
-    @IBAction func ButtonClicked(_ sender: Any) {
-        print("Button clicked!")
-        
-        if let name = textField.text {
-        
-            label.text = "Hello " + name
+    @IBAction func clickButton(_ sender: Any) {
+        if let age = Int(textField.text!) {
+            let dogAge = age * 7
+            
+            label.text = "Your dog is " + String(dogAge) + " in dog years"
         }
         
     }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
