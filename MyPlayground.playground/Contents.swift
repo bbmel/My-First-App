3b34c6da32e2aef3f7c611b9bd915bad670783c4
Mel <mmgravier@gmail.com>
2017-11-19 12:39:26 -0500

    added the playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Declaring variables
+let myVar = "trippy lil hippie"
+print("Let's concat" + " " + myVar)
+
+var changeMyVar = "happy hippie"
+changeMyVar = "bbmel"
+print(changeMyVar)
+
+// Integers (whole numbers)
+
+// cannot change let variables
+let myInt = 8
+print("The value of myInt is \(myInt)")
+
+// can change var variables
+var changeInt = 10
+changeInt = 9
+print("The value of changeInt is \(changeInt)")
+
+// lil challenge
+let myName = "Mel"
+let myAge = "25"
+print("My name is " + myName + " and I am \(myAge) years old")
+
+// Doubles and floats (you can't combine two different types btw)
+
+var a = 8.73
+var aSpecified:Double = 6.66
+var b = 7.77
+var bSpecified:Float = 8.88
+
+
